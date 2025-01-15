38e5d19a5d51debaf6fba489f8f771153d9d96f3
Student <student@abv.bg>
2025-01-15 11:31:52 +0200

    Division, abs Subtraction added; Introduced authorization

--- Startup.cs
+++ Startup.cs
@@ -4,10 +4,25 @@ namespace GitExercise
 {
     public class Startup
     {
+        private static bool CheckCredentials()
+        {
+            Console.Write("Enter password to gain access: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+            return password == Password;
+        }
+        private const string Password = "abcd1234";
         public static void Main()
         {
+            bool isAuthorized = CheckCredentials();
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Access denied.");
+                Console.ReadKey(intercept: true);
+                return;
+            }
             Console.WriteLine("Console Calculator App");
-            Console.WriteLine(new string('-', 15));
+            Console.WriteLine(new string(c:'-',count: 15));
 
             Console.Write("a = ");
             double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
